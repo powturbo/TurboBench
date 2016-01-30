865db8f55d989af03892e7b6afebdeb62cf6e4a5
powturbo <powturbo@gmail.com>
2016-01-30 13:55:46 +0100

    .

--- lzoma_/lzoma.c
+++ lzoma_/lzoma.c
@@ -1,39 +1,77 @@
-#include "lzoma-0.2.c"
-#include "lzoma.h"
-
-int lzomapack(unsigned char *in, int inlen, unsigned char *out, int lev) { 
-  char C[2],*c=C; C[0]=lev+48; C[1]=0;
- 
-		int A[]={15,2,999},i=0,a=0,l,n=0,bs;
-		for(l=sizeof(A)/sizeof(A[0]);*++c&&i<l;)
-		  if(*c<58&&*c>47){if(a<9)n=n*10+*c-48,a++;}
-		  else{ if(a) A[i]=n;i++;a=n=0;}
-		if(a&&i<l)A[i]=n;
-
-		bs=(A[0]&511)+1<<20; if(bs>inlen)bs=inlen;
-        //int bs = inlen+(1<<24);
-        if(bs<65536)bs=65536;
-		l = sizeof(int);
-		T=malloc(bs*(l*5+1));     if(!T) return 0;
-		same=malloc(bs*l*9+l*2);  if(!same) return free(T),0;
-
-		O=T+bs, SA=(int*)O, ISA=SA+bs, LCP=ISA+bs, Prev=LCP+bs, Next=Prev+bs;
-		same3=same+bs, samelen=same3+bs, useOlz=samelen+bs, useOlz2=useOlz+bs, olzLen=useOlz2+bs, olzLen2=olzLen+bs, RLE=olzLen2+bs, cacheRLE=RLE+bs+1;
-  memcpy(T, in, inlen);
-  unsigned outlen = pack(inlen); 
-  memcpy(out, O, outlen);
-  free(T);
-  free(same);
-  return outlen;
-}
 
 int lzomaunpack( unsigned char *in, int inlen, unsigned char *out, int outlen) {
-  if(!(T=malloc(outlen*2+9))) return 0;
-  O=T+outlen+9;
-  memcpy(T,in,inlen);
-  unpack(T, O, outlen);
-  memcpy(out,O, outlen);
-  free(T);
-  return outlen;
+  unsigned char *ifd=in,ifd_=in+inlen,*ofd=out;
+  int n,n_unp;
+  char shift;
+
+  //ifd=open(argv[1],O_RDONLY|O_BINARY);
+  //ofd=open(argv[2],O_WRONLY|O_TRUNC|O_CREAT|O_BINARY,511);
+  int current_history = 0;
+  int ofs = 0;
+  int use_e8=0;
+  uint8_t header[8];
+  _fread(header,1,8,ifd,ifd_);
+  if (header[0] != (AuthorID >> 8) ||
+      header[1] != (AuthorID & 0xFF) ||
+      header[2] != AlgoID[0] ||
+      header[3] != AlgoID[1] ||
+      header[4] != AlgoID[2] ||
+      header[5] != AlgoID[3] ||
+      header[6] != Version) {
+    fprintf(stderr, "Unsupported compressed data format\n");
+    return 1;
+  }
+  int block_size = 32*1024 << (header[7] & 0xF);
+  int history_size = block_size << 4;
+  in_buf = (uint8_t *)malloc(block_size);
+  out_buf = (uint8_t *)malloc(history_size); // history is 16*block_size
+
+  uint32_t blk;
+  while(_fread(&blk,1,4,ifd,ifd_)==4) {
+    //if (use_e8) e8(out_buf,n_unp);
+    n = blk & (block_size-1);
+    if (blk & BLOCK_STORED) {
+      n_unp = n;
+    } else if (blk & BLOCK_LAST) {
+      _fread(&n_unp,1,4,ifd,ifd_);
+    } else {
+      n_unp = block_size;
+    }
+    /*
+    if (n != n_unp && !current_history) 
+      read(ifd,&use_e8,1);
+    else
+      use_e8 = 0;
+    */
+    //long unsigned tsc = (long unsigned)__rdtsc();
+    if (n == n_unp) {
+      _fread(out_buf,1,n_unp,ifd,ifd_);
+      _fwrite(out_buf+ofs,1,n_unp,ofd);
+    } else {
+      _fread(in_buf,1,n,ifd,ifd_);
+#ifdef ASM_X86
+#error Asm version not yet updated for recent format changes. Please use C version right now.
+      unpack_x86(in_buf, out_buf, n_unp);
+#else
+      unpack_c(current_history, in_buf, out_buf+ofs, out_buf, n_unp);
+#endif
+      //tsc=(long unsigned)__rdtsc()-tsc;
+      //printf("tsc=%lu\n",tsc);
+      //if (use_e8) e8back(out_buf,n_unp);
+      _fwrite(out_buf+ofs,1,n_unp,ofd);
+    }
+    if (blk & BLOCK_LAST)
+      break;
+    ofs+=n_unp;
+    ofs &= (history_size-1);
+    current_history += n_unp;
+    if (current_history > history_size-block_size)
+      current_history = history_size-block_size;
+  }
+
+  /*close(ifd);
+  close(ofd);
+  return 0;*/
+  return ifd - in;
 }
 
