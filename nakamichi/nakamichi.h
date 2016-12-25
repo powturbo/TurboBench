854edeaf72befc2358567f012c4cae020d184dbc
x <powturbo@gmail.com>
2016-12-25 14:12:10 +0100

    .

--- nakamichi/nakamichi.h
+++ nakamichi/nakamichi.h
@@ -2,7 +2,7 @@
 extern "C" {
 #endif
 #include <stdint.h>
-unsigned int NakaCompress(char* ret, char* src, unsigned int srcSize);
+uint64_t NakaCompress(char* ret, char* src, unsigned int srcSize);
 uint64_t NakaDecompress (char* ret, char* src, uint64_t srcSize);
 #ifdef __cplusplus
 }
