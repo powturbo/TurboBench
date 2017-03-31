008b79f43abea893a769a11bdfadb4e082a6139e
x <powturbo@gmail.com>
2017-03-31 11:09:02 +0200

    TurboBench: Config/Platform

--- conf.h
+++ conf.h
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) powturbo 2013-2016
+    Copyright (C) powturbo 2013-2017
     GPL v2 License
   
     This program is free software; you can redistribute it and/or modify
@@ -38,14 +38,16 @@
 #define popcnt64(_x_) 	__builtin_popcountll(_x_)
 
     #if defined(__i386__) || defined(__x86_64__)
+//__bsr32     1:0,2:1,3:1,4:2,5:2,6:2,7:2,8:3,9:3,10:3,11:3,12:3,13:3,14:3,15:3,16:4,17:4,18:4,19:4,20:4,21:4,22:4,23:4,24:4,25:4,26:4,27:4,28:4,29:4,30:4,31:4,32:5
+//bsr32:  0:0,1:1,2:2,3:2,4:3,5:3,6:3,7:3,8:4,9:4,10:4,11:4,12:4,13:4,14:4,15:4,16:5,17:5,18:5,19:5,20:5,21:5,22:5,23:5,24:5,25:5,26:5,27:5,28:5,29:5,30:5,31:5,32:6,
 static inline int    __bsr32(               int x) {             asm("bsr  %1,%0" : "=r" (x) : "rm" (x) ); return x; }
 static inline int      bsr32(               int x) { int b = -1; asm("bsrl %1,%0" : "+r" (b) : "rm" (x) ); return b + 1; }
 static inline int      bsr64(unsigned long long x) { return x?64 - __builtin_clzll(x):0; }
-#define bsr16(_x_)     bsr32(_x_)
 
 static inline unsigned rol32(unsigned x, int s) { asm ("roll %%cl,%0" :"=r" (x) :"0" (x),"c" (s)); return x; }
 static inline unsigned ror32(unsigned x, int s) { asm ("rorl %%cl,%0" :"=r" (x) :"0" (x),"c" (s)); return x; }
-
+static inline unsigned long long rol64(unsigned long long x, int s) { asm ("rolq %%cl,%0" :"=r" (x) :"0" (x),"c" (s)); return x; }
+static inline unsigned long long ror64(unsigned long long x, int s) { asm ("rorq %%cl,%0" :"=r" (x) :"0" (x),"c" (s)); return x; }
     #else
 static inline int    __bsr32(unsigned x          ) { return   31 - __builtin_clz(  x); }
 static inline int      bsr32(int x               ) { return x?32 - __builtin_clz(  x):0; }
@@ -53,6 +55,8 @@ static inline int      bsr64(unsigned long long x) { return x?64 - __builtin_clz
 
 static inline unsigned rol32(unsigned x, int s) { return x << s | x >> (32 - s); }
 static inline unsigned ror32(unsigned x, int s) { return x >> s | x << (32 - s); }
+static inline unsigned rol64(unsigned x, int s) { return x << s | x >> (64 - s); }
+static inline unsigned ror64(unsigned x, int s) { return x >> s | x << (64 - s); }
     #endif
 
 #define ctz64(_x_) __builtin_ctzll(_x_)
@@ -60,10 +64,10 @@ static inline unsigned ror32(unsigned x, int s) { return x >> s | x << (32 - s);
 #define clz64(_x_) __builtin_clzll(_x_)
 #define clz32(_x_) __builtin_clz(_x_)
 
-#if __GNUC_MINOR__ < 8
-#define bswap16(_x_) ((_x_)<<8|(_x_)>>8)
-#else
+#if __GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 8
 #define bswap16(x) __builtin_bswap16(x)
+#else
+static inline unsigned short bswap16(unsigned short x) { return __builtin_bswap32(x << 16); }
 #endif
 #define bswap32(x) __builtin_bswap32(x)
 #define bswap64(x) __builtin_bswap64(x)
@@ -100,8 +104,13 @@ static inline int ctz32(unsigned           x) { unsigned      z = 0; _BitScanRev
 #define strncasecmp _strnicmp
   #endif 
 
+#define bsr8(_x_)  bsr32(_x_)
+#define bsr16(_x_) bsr32(_x_)
+#define ctz8(_x_)  ctz32(_x_)
 #define ctz16(_x_) ctz32(_x_)
-#define clz16(_x_) clz32(_x_)
+#define clz8(_x_)  (clz32(_x_)-8)
+#define clz16(_x_) (clz32(_x_)-16)
+
 //--------------- Unaligned memory access -------------------------------------
 /*# || defined(i386) || defined(_X86_) || defined(__THW_INTEL)*/
   #if defined(__i386__) || defined(__x86_64__) || \
@@ -111,8 +120,8 @@ static inline int ctz32(unsigned           x) { unsigned      z = 0; _BitScanRev
     defined(__ARM_ARCH_4__) || defined(__ARM_ARCH_4T__) || \
     defined(__ARM_ARCH_5__) || defined(__ARM_ARCH_5T__) || defined(__ARM_ARCH_5TE__) || defined(__ARM_ARCH_5TEJ__) || \
     defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__)  || defined(__ARM_ARCH_6T2__) || defined(__ARM_ARCH_6Z__)   || defined(__ARM_ARCH_6ZK__)
-#define ctou16(_cp_) *(unsigned short *)(_cp_)
-#define ctou32(_cp_) *(unsigned       *)(_cp_)
+#define ctou16(_cp_) (*(unsigned short *)(_cp_))
+#define ctou32(_cp_) (*(unsigned       *)(_cp_))
 
     #if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__)
 #define ctou64(_cp_)       (*(unsigned long long *)(_cp_))
@@ -171,7 +180,7 @@ static inline unsigned long long ctou64(const void *cp) { unsigned long long x; 
   #endif
 #endif
 
-//---------------------misc ---------------------------------------------------   
+//---------------------misc ---------------------------------------------------
 #define SIZE_ROUNDUP(_n_, _a_) (((size_t)(_n_) + (size_t)((_a_) - 1)) & ~(size_t)((_a_) - 1))
   
 #define TEMPLATE2_(_x_, _y_) _x_##_y_
