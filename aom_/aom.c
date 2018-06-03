87ff15a239e681542c474c2e6ede0e045d76fd47
x <powturbo@gmail.com>
2018-06-03 12:51:56 +0200

    .

--- aom_/aom.c
+++ aom_/aom.c
@@ -1,11 +1,11 @@
 #include "../aom/aom_dsp/entenc.h"
 #include "../aom/aom_dsp/entdec.h"
 
-#define MNDEF0(_m_) mbu _m_[17]; 		 
-#define MNDEF1(_m_,_n_) mbu _m_[_n_][17]
+#define MNDEF0(_m_)     aom_cdf_prob      _m_[17] 		 
+#define MNDEF1(_m_,_n_) aom_cdf_prob _m_[_n_][17]
 
-#define MNDEC0(_m_)     { int j;                          for(j = 0; j <= 16; j++) _m_[j]    = (j+1)*0x800; }
-#define MNDEC1(_m_,_n_) { int i,j; for(i=0; i < _n_; i++) for(j = 0; j <= 16; j++) _m_[i][j] = (j+1)*0x800; }
+#define MNDEC0(_m_)     { int j;                          for(j = 0; j <= 16; j++) _m_[j]    = OD_ICDF((j+1)*0x800); }
+#define MNDEC1(_m_,_n_) { int i,j; for(i=0; i < _n_; i++) for(j = 0; j <= 16; j++) _m_[i][j] = OD_ICDF((j+1)*0x800); }
 
 #define mn4c(_m_,_y_)         { od_ec_encode_cdf_q15(&enc, _y_, _m_, 16); update_cdf(_m_, _y_, 16); }
 #define mn8c(_mh_, _ml_, _x_) { unsigned _yh = (_x_)>>4,_yl=(_x_) & 0xf; mn4c(_mh_, _yh); mn4c(_ml_[_yh], _yl); }
@@ -16,8 +16,8 @@
 int aomenc( unsigned char *in, unsigned inlen,  unsigned char *out, unsigned outsize) { 
   unsigned char *ip,*op; 														
   unsigned s;
-  MNDEF0(mb0); MNDEF1(mb,16);
-  MNDEC0(mb0); MNDEC1(mb,16);
+  MNDEF0(mh); MNDEF1(ml,16);
+  MNDEC0(mh); MNDEC1(ml,16);
 
   od_ec_enc enc;
   od_ec_enc_init(&enc, inlen);
@@ -31,8 +31,8 @@ int aomenc( unsigned char *in, unsigned inlen,  unsigned char *out, unsigned out
 
 int aomdec( unsigned char *in, unsigned inlen, unsigned char *out, unsigned outlen ) { 
   unsigned char *op; 										
-  MNDEF0(mb0); MNDEF1(mb,16);
-  MNDEC0(mb0); MNDEC1(mb,16);
+  MNDEF0(mh); MNDEF1(ml,16);
+  MNDEC0(mh); MNDEC1(ml,16);
   od_ec_dec dec;
   od_ec_dec_init(&dec, in, inlen);
   for(op=out; op < out+outlen;op++) mn8d(mh,ml,*op);
