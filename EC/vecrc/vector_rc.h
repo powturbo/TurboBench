a861bc8df83e6cec04c9099701332a6d9e5b6ab7
x <powturbo@gmail.com>
2020-02-19 16:24:05 +0100

    Range Coder:Eugene Shelwien

---
+++ EC/vecrc/vector_rc.h
@@ -0,0 +1,8 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+int vecrcenc(unsigned char *ip, int ilen, unsigned char *op);
+int vecrcdec(unsigned char *ip, int olen, unsigned char *op);
+#ifdef __cplusplus
+}
+#endif
