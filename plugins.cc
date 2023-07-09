ecbcf0efed1a1a48ceb1b9bbbdcbc8ae57c63ce4
x <powturbo@gmail.com>
2023-07-09 10:29:36 +0200

    TurboBench: Compression codecs

--- plugins.cc
+++ plugins.cc
@@ -21,7 +21,7 @@
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-//      TurboBench: plugins.cc - compressor plugins
+//     TurboBench: plugins.cc - compressor plugins
 enum {
 #define _MEMCPY 1
  P_LMCPY,   // must be 0
