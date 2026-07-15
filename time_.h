ddf9968b2ae8286ba8babc7ad6c2d4c5a3bcf7dc
powturbo <powturbo@gmail.com>
2026-07-15 11:16:43 +0200

    TurboBench: Benchmark App: include

--- time_.h
+++ time_.h
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) powturbo 2013-2023
+    Copyright (C) powturbo 2013-2026
     GPL v2 License
 
     This program is free software; you can redistribute it and/or modify
@@ -22,22 +22,29 @@
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
 //      time_.h : parameter free high precision time/benchmark functions
+#define _POSIX_C_SOURCE 200809L // clocktime
+#include <stdio.h>
 #include <time.h>
 #include <float.h>
+
   #ifdef _WIN32
 #include <windows.h>
     #ifndef sleep
 #define sleep(n) Sleep((n) * 1000)
     #endif
 #define uint64_t unsigned __int64
-
   #else
 #include <stdint.h>
 #include <unistd.h>
 #define Sleep(ms) usleep((ms) * 1000)
+#ifdef __riscv_v
+#define sleep(x)  
+#endif
   #endif
 
-#if defined (__i386__) || defined( __x86_64__ )  // ------------------ rdtsc --------------------------
+#define TM_MAN 9
+
+#if defined (__i386__) || defined( __x86_64__ ) // ------------------ rdtsc --------------------------
   #ifdef _MSC_VER
 #include <intrin.h> // __rdtsc
   #else
@@ -80,7 +87,7 @@
 #define RDTSC_INI(_c_) RDTSC(_c_)
 #else                                          // ------------------ time --------------------------
 #define RDTSC_INI(_c_)
-#define RDTSC(_c_)
+#define RDTSC(_c_) __rdtsc()       
 #endif
 
 #ifndef TM_F
@@ -91,20 +98,21 @@
 #define TM_M   (CLOCKS_PER_SEC*1000000ull)
 #define TM_PRE 4
 #define TM_MBS "cycle/byte"
-static double TMBS(unsigned long long l, double t) { return (double)t/(double)l; }
+static double TMBS(size_t l, double t) { return t/l; }
 
 typedef uint64_t tm_t;
 static tm_t   tmtime()                      { uint64_t c; RDTSC(c); return c; }
 static tm_t   tminit()                      { uint64_t c; __asm volatile("" ::: "memory"); RDTSC_INI(c); return c; }
 static double tmdiff(tm_t start, tm_t stop) { return (double)(stop - start); }
 static int    tmiszero(tm_t t)              { return !t; }
+
 #else          //---------------------- time -----------------------------------
 #define TM_M   1
 #define TM_PRE 2
 #define TM_MBS "MB/s"
-static double TMBS(unsigned long long l, double t) { return (l/t)/1000000.0; }
+static double TMBS(size_t l, double t) { return (l/t)/1000000.0; }
 
-  #ifdef _WIN32 //-------- windows 
+  #ifdef _WIN32 //-------- windows
 static LARGE_INTEGER tps;
 
 typedef unsigned __int64 tm_t;
@@ -113,7 +121,7 @@ static tm_t   tminit()                      { tm_t t0,ts; QueryPerformanceFreque
 static double tmdiff(tm_t start, tm_t stop) { return (double)(stop - start)/tps.QuadPart; }
 static int    tmiszero(tm_t t)              { return !t; }
   #else        // Linux & compatible / MacOS
-    #ifdef __APPLE__
+    #ifdef __APPLE__  //TODO: #include <mach/mach_time.h> uint64_t t = mach_absolute_time();
 #include <AvailabilityMacros.h>
       #ifndef MAC_OS_X_VERSION_10_12
 #define MAC_OS_X_VERSION_10_12 101200
@@ -133,22 +141,34 @@ int clock_gettime(int /*clk_id*/, struct timespec* t) {
 }
       #endif
     #endif
-    
-typedef struct timespec tm_t;   
-static tm_t   tmtime()                      { struct timespec tm; clock_gettime(CLOCK_MONOTONIC, &tm); return tm; }
+
+    #ifdef CLOCK_MONOTONIC
+typedef struct timespec tm_t;
+static tm_t tmtime() { struct timespec tm; clock_gettime(CLOCK_MONOTONIC, &tm); return tm; }
+static int    tmiszero(tm_t t)              { return !(t.tv_sec|t.tv_nsec); }
 static double tmdiff(tm_t start, tm_t stop) { return (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec)/1e9f; }
 static tm_t   tminit()                      { tm_t t0 = tmtime(),t; while(!tmdiff(t = tmtime(),t0)) {}; return t; }
-static int    tmiszero(tm_t t)              { return !(t.tv_sec|t.tv_nsec); }
+    #else
+typedef unsigned long long tm_t;
+static tm_t tps;
+
+static tm_t   tmtime()                      { return __builtin_readcyclecounter(); }
+static int    tmiszero(tm_t t)              { return !t; }
+static double tmdiff(tm_t start, tm_t stop) { return (double)(stop - start); }
+static tm_t   tminit()                      { tm_t t0,ts; tps = __builtin_readcyclecounter(); t0 = tmtime(); while((ts = tmtime()) == t0) {}; return ts; }
+    #endif	
   #endif
-#endif 
+#endif
 
 //---------------------------------------- bench ----------------------------------------------------------------------
-// for each a function call is repeated until exceeding tm_tx seconds.
-// A run duration is always tm_tx seconds
-// The number of runs can be set with the program options  -I and -J (specify -I15 -J15 for more precision)
+// For each function, the call is repeated until the total time exceeds tm_tx seconds.
+// Then every run is timed for tm_tx seconds.
+// The number of runs can be adjusted via options -I and -J (use -I15 -J15 for better precision).
 
 // sleep after each 8 runs to avoid cpu throttling.
-#define TMSLEEP do { tm_T = tmtime(); if(tmiszero(tm_0)) tm_0 = tm_T; else if(tmdiff(tm_0, tm_T) > tm_TX) { if(tm_verbose>2) { printf("S \b\b");fflush(stdout); } sleep(tm_slp); tm_0=tmtime();} } while(0)
+#define TMSLEEP do { tm_T = tmtime(); if(tmiszero(tm_0)) tm_0 = tm_T; else if(tmdiff(tm_0, tm_T) > tm_TX) { if(tm_verbose>1) { printf("S \b\b");fflush(stdout); } sleep(tm_slp); tm_0=tmtime();} } while(0)
+
+#define TM_BACK(_n_) { for(int i=0; i < (_n_); i++) printf("\b");fflush(stdout); }
 
 // benchmark loop
 #define TMBEG(_tm_Reps_) { unsigned _tm_r,_tm_c = 0,_tm_R,_tm_Rx = _tm_Reps_,_tm_Rn = _tm_Reps_; double _tm_t;\
@@ -165,42 +185,44 @@ static int    tmiszero(tm_t t)              { return !(t.tv_sec|t.tv_nsec); }
     if(_tm_t < tm_tm) { if(tm_tm == DBL_MAX) { tm_rm = _tm_r; _tm_Rn = tm_TX/_tm_t; _tm_Rn = _tm_Rn<_tm_Rx?_tm_Rn:_tm_Rx; /*printf("repeats=%u,%u,%.4f ", _tm_Rn, _tm_Rx, _tm_t);*/ } \
 	  tm_tm = _tm_t; _tm_c++;\
     } else if(_tm_t > tm_tm*1.15) TMSLEEP;/*force sleep at 15% divergence*/\
-    if(tm_verbose>2) { printf("%8.*f %2d_%.2d\b\b\b\b\b\b\b\b\b\b\b\b\b\b",TM_PRE, TMBS(_size_, tm_tm/tm_rm),_tm_R+1,_tm_c),fflush(stdout); }\
+    if(tm_verbose>1) { printf("%*.*f %2d_%.2d\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", TM_MAN,TM_PRE, TMBS(_size_, tm_tm/tm_rm),_tm_R+1,_tm_c); fflush(stdout);  }\
     if((_tm_R & 7)==7) sleep(tm_slp); /*pause 20 secs after each 8 runs to avoid cpu throttling*/\
   }\
 }
 
-static unsigned tm_rep = 1u<<30, tm_Rep = 3, tm_Rep2 = 3, tm_rm, tm_RepMin = 1, tm_slp = 20, tm_verbose = 3;
+static unsigned tm_rep = 1u<<30, tm_Rep = 3, tm_Rep2 = 3, tm_rm, tm_RepMin = 1, tm_slp = 20, tm_verbose = 2;
 static tm_t tm_0, tm_T;
 static double tm_tm, tm_tx = 1.0*TM_M, tm_TX = 60.0*TM_M;
 
-static void tm_init(unsigned _tm_Rep, int _tm_verbose) { tm_verbose = _tm_verbose; if(_tm_Rep) tm_Rep = _tm_Rep; }
+static void tm_init(int _tm_Rep, int _tm_verbose) { tm_verbose = _tm_verbose; if(_tm_Rep) tm_Rep = _tm_Rep; }
 
 #define TM(_name_, _efunc_, _size_, _len_, _dfunc_) do {\
-  TMBEG(tm_Rep) _efunc_; if(_size_ && !(_tm_R|_tm_r) ) pr(_len_,_size_); TMEND(_size_);\
+  TMBEG(tm_Rep) _efunc_; if(_size_ && !(_tm_R|_tm_r) && tm_verbose) pr(_len_,_size_); TMEND(_size_);\
   double dm = tm_tm, dr = tm_rm; \
-  if(tm_verbose>2) printf("%8.*f      \b\b\b\b\b", TM_PRE, TMBS(_size_, dm/dr) );\
-  else if(tm_verbose) printf("%8.*f      ", TM_PRE, TMBS(_size_, dm/dr) );\
+  if(tm_verbose>1)    { printf("%*.*f      \b\b\b\b\b", TM_MAN, TM_PRE, TMBS(_size_, dm/dr) ); fflush(stdout); }\
+  else if(tm_verbose) { printf("%*.*f      ",  TM_MAN, TM_PRE, TMBS(_size_, dm/dr) );          fflush(stdout); }\
   \
   TMBEG(tm_Rep2) _dfunc_; TMEND(_size_);\
-  dm = tm_tm; dr = tm_rm; if(tm_verbose>2) printf("%8.*f      \b\b\b\b\b", TM_PRE,TMBS(_size_, dm/dr) );else if(tm_verbose) printf("%8.*f      ", TM_PRE,TMBS(_size_, dm/dr) );\
-  if(tm_verbose>3) printf("%s ", _name_?_name_:#_efunc_);\
+  dm = tm_tm; dr = tm_rm; \
+  if(tm_verbose>1)    { printf("%*.*f      \b\b\b\b\b", TM_MAN, TM_PRE, TMBS(_size_, dm/dr) ); fflush(stdout); }\
+  else if(tm_verbose) { printf("%*.*f      ", TM_MAN, TM_PRE,TMBS(_size_, dm/dr) );            fflush(stdout); }\
+  if(tm_verbose)    { printf("%s ", _name_?_name_:#_efunc_); fflush(stdout); }\
 } while(0)
 
 #define TM0(_name_, _efunc_, _size_, _len_) do {\
-  TMBEG(tm_Rep) _efunc_; if(_size_ && !(_tm_R|_tm_r) ) pr(_len_,_size_); TMEND(_size_);\
+  TMBEG(tm_Rep) _efunc_; if(_size_ && !(_tm_R|_tm_r) && tm_verbose) pr(_len_,_size_); TMEND(_size_);\
   double dm = tm_tm, dr = tm_rm; \
-  if(tm_verbose>2) printf("%8.*f      \b\b\b\b\b", TM_PRE, TMBS(_size_, dm/dr) );\
-  else if(tm_verbose) printf("%8.*f      ", TM_PRE, TMBS(_size_, dm/dr) );\
+  if(tm_verbose>1)    { printf("%*.*f      \b\b\b\b\b", TM_MAN, TM_PRE, TMBS(_size_, dm/dr) ); fflush(stdout); }\
+  else if(tm_verbose) { printf("%*.*f      ", TM_MAN, TM_PRE, TMBS(_size_, dm/dr) );           fflush(stdout); }\
   \
-  if(tm_verbose>3) printf("%s ", _name_?_name_:#_efunc_);\
+  if(tm_verbose) { printf("%s ", _name_?_name_:#_efunc_); fflush(stdout); }\
 } while(0)
 
-static void pr(unsigned l, unsigned n) {
+static void pr(size_t l, size_t n) {
   double r = (double)l*100.0/n;
-  if(r>0.1)  printf("%10u %6.2f%%   ", l, r);
-  else if(r>0.01) printf("%10u %7.3f%%  ", l, r);
-  else printf("%10u %8.4f%% ", l, r); fflush(stdout); 
+  if(r>0.1)       printf("%11llu %6.2f%%   ", l, r);
+  else if(r>0.01) printf("%11llu %7.3f%%  ",  l, r);
+  else            printf("%11llu %8.4f%% ",   l, r); fflush(stdout);
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------
@@ -227,7 +249,7 @@ static unsigned argtoi(char *s, unsigned def) {
   }
   return n*f;
 }
-static uint64_t argtol(char *s) {
+static uint64_t argtol(char *s, uint64_t def) {
   char *p;
   uint64_t n = strtol(s, &p, 10),f=1;
   switch(*p) {
@@ -238,8 +260,8 @@ static uint64_t argtol(char *s) {
     case 'm': f = Mb; break;
     case 'g': f = Gb; break;
     case 'B': return n; break;
-    case 'b': return 1u << n;
-    default:  f = Mb;
+    case 'b': def = 0;
+    default:  if(!def) return n>=64?0xffffffffffffffffu:(1ull << n); f = def;
   }
   return n*f;
 }
@@ -257,5 +279,4 @@ static uint64_t argtot(char *s) {
   return n*f;
 }
 
-static void memrcpy(unsigned char *out, unsigned char *in, unsigned n) { unsigned i; for(i = 0; i < n; i++) out[i] = ~in[i]; }
-
+static void memrcpy(unsigned char *out, unsigned char *in, unsigned n) { int i; for(i = 0; i < n; i++) out[i] = ~in[i]; }
