872e0cdbc9141440597398c55ef4960d75bde9de
powturbo <powturbo@gmail.com>
2026-07-17 19:15:09 +0200

    2026.07.17

---
+++ pcodec_/cpcodec.h
@@ -0,0 +1,24 @@
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include "cpcodec_generated.h"
+
+// cbindgen can only handle literal constants, which isn't helpful when these
+// come from public constants in the pco crate, so we just redefine them here.
+#define PCO_TYPE_U32 1
+#define PCO_TYPE_U64 2
+#define PCO_TYPE_I32 3
+#define PCO_TYPE_I64 4
+#define PCO_TYPE_F32 5
+#define PCO_TYPE_F64 6
+#define PCO_TYPE_U16 7
+#define PCO_TYPE_I16 8
+#define PCO_TYPE_F16 9
+#define PCO_TYPE_U8 10
+#define PCO_TYPE_I8 11
+
+#if defined (__cplusplus)
+}
+#endif
