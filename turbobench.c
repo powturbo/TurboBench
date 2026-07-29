cdb14cc141c4cbae4da48422b52e261f2b6ae6ea
powturbo <powturbo@gmail.com>
2026-07-29 23:46:50 +0200

    TurboBench: Benchmark App

--- turbobench.c
+++ turbobench.c
@@ -209,8 +209,8 @@ static __attribute__((constructor)) void mem_init(void) {
   mem_memalign = dlsym(RTLD_NEXT, "memalign");
   mem_posix_memalign = dlsym(RTLD_NEXT, "posix_memalign");
 
-  if(!mem_malloc || !mem_calloc || !mem_realloc || !mem_free || !mem_memalign)
-    die("malloc not found. mem_malloc:%d mem_calloc:%d mem_realloc:%d mem_free:%d mem_memalign:%d\n", mem_malloc?1:0, mem_calloc?1:0, mem_realloc?1:0, mem_free?1:0, mem_memalign?1:0);
+  if(!mem_malloc || !mem_calloc || !mem_realloc || !mem_free)
+    die("malloc not found. mem_malloc:%d mem_calloc:%d mem_realloc:%d mem_free:%d\n", mem_malloc?1:0, mem_calloc?1:0, mem_realloc?1:0, mem_free?1:0);
 }
 
 void *malloc(size_t size) {
