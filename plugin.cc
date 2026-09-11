bb795b70b18de21471844dacd8911ad56c24f2da
powturbo <powturbo@gmail.com>
2026-09-11 10:55:32 +0200

    TurboBench: Compression codecs

--- plugin.cc
+++ plugin.cc
@@ -37,6 +37,7 @@ enum {
 #define _MEMCPY 1
  P_LMCPY,   // must be 0
  P_MCPY,    // must be 1
+
 #ifndef _AOCL
 #define _AOCL 0
 #endif
@@ -94,7 +95,6 @@ enum {
 #ifndef _DAALA
 #define _DAALA 0
 #endif
-
  P_DAALA,
 #ifndef _DENSITY
 #define _DENSITY 0
@@ -113,25 +113,30 @@ enum {
 #define _FLZMA2 0
 #endif
  P_FLZMA2,
+
 #ifndef _GIPFELI
 #define _GIPFELI 0
 #endif
-
  P_GIPFELI,
 #ifndef _GLZA
 #define _GLZA 0
 #endif
  P_GLZA,
+
 #ifndef _HEATSHRINK
 #define _HEATSHRINK 0
 #endif
-
  P_HEATSHRINK,
+
+#ifndef _IGUANA_L
+#define _IGUANA_L 0
+#endif
+ P_IGUANA,
 #ifndef _ISA_L
 #define _ISA_L 0
 #endif
-
  P_ISA_L,
+
 #ifndef _KANZI
 #define _KANZI 0
 #endif
@@ -148,7 +153,6 @@ enum {
 #define _DIVBWT 0
 #endif
  P_DIVBWT,    //bwt
-
 #ifndef _LIB
 #define _LIB 0
 #endif
@@ -165,11 +169,6 @@ enum {
 #define _LIBLZG 0
 #endif
  P_LIBLZG,
- 
-#ifndef _MEMLZ
-#define _MEMLZ 0
-#endif
- P_MEMLZ,
  
 #ifndef _LZ4
 #define _LZ4 0
@@ -233,6 +232,11 @@ enum {
  P_LZSSE2,
  P_LZSSE4,
  P_LZSSE8,
+
+#ifndef _MEMLZ
+#define _MEMLZ 0
+#endif
+ P_MEMLZ,
 #ifndef _MINIZ
 #define _MINIZ 0
 #endif
@@ -687,6 +691,68 @@ static size_t cscwrite(MemISeqOutStream *so, const void *out, size_t outlen) {
 
   #if _HEATSHRINK
 #include "heatshrink_/heatshrink.h"
+  #endif
+
+  #if _IGUANA
+#include "iguana/iguana/encoder.h"
+#include "iguana/iguana/decoder.h"
+#include "iguana/iguana/output_stream.h"
+#include "iguana/iguana/input_stream.h"
+#include "iguana/iguana/entropy.h"
+
+//namespace ErrorCodes { extern const int CANNOT_COMPRESS; }
+//unsigned getMaxCompressedDataSize(UInt32 uncompressed_size) { return uncompressed_size + ENCODER_OVERHEAD; }
+
+#define ENCODER_OVERHEAD 1024
+unsigned IguanaComp(const char * source, unsigned source_size, char *dest) {
+  iguana::output_stream out;
+  out.reserve(source_size + ENCODER_OVERHEAD);
+
+  const iguana::encoder::part part {
+    .m_data = reinterpret_cast<const std::uint8_t *>(source),
+    .m_size = source_size,
+        /// Per-substream entropy stage: 32-way interleaved 8-bit rANS (the reference default).
+    .m_entropy_mode = iguana::entropy_mode::ans32,
+        /// Full Iguana pipeline: LZ structural compression followed by the entropy stage above.
+    .m_encoding = iguana::encoding::iguana,
+        /// With a threshold of 1.0 the encoder stores a block (or substream) verbatim whenever
+        /// compression would not make it smaller, so the output never grows beyond the input by
+        /// more than the small control header.
+    .m_rejection_threshold = iguana::encoder::default_rejection_threshold,
+  };
+  //try {
+    iguana::encoder encoder;
+    encoder.encode(out, part);
+  /*}
+  catch (const std::exception & e) {
+    throw Exception(ErrorCodes::CANNOT_COMPRESS, "Cannot compress with Iguana codec: {}", e.what());
+  }*/
+  const std::size_t compressed_size = out.size();
+    /*if (compressed_size > getMaxCompressedDataSize(source_size))
+        throw Exception(ErrorCodes::CANNOT_COMPRESS,
+            "Iguana codec produced {} bytes, which exceeds the reserved size {} for an input of {} bytes",
+            compressed_size, getMaxCompressedDataSize(source_size), source_size);*/
+
+  memcpy(dest, out.data(), compressed_size);
+  return static_cast<unsigned>(compressed_size);
+}
+
+unsigned IguanaDecomp(const char *source, unsigned source_size, char *dest, unsigned uncompressed_size) {
+  iguana::output_stream out;
+  out.reserve(uncompressed_size);
+  //try {
+    iguana::decoder decoder;
+    iguana::input_stream in(reinterpret_cast<const std::uint8_t *>(source), source_size);
+    decoder.decode(out, in);
+  //}
+  /*catch (const std::exception & e) {
+    throw Exception(decompression_error_code, "Cannot decompress Iguana-encoded data: {}", e.what());
+  }
+  if (out.size() != uncompressed_size)
+    throw Exception(decompression_error_code, "Iguana codec decompressed {} bytes, but {} were expected", out.size(), uncompressed_size);*/
+  memcpy(dest, out.data(), out.size());
+  return static_cast<unsigned>(out.size());
+}
   #endif
 
   #if _ISA_L
@@ -1648,6 +1714,7 @@ struct plugs plugs[] = {
   
   { P_HEATSHRINK,    "heatshrink",    _HEATSHRINK,"heatshrink",              "" },
   
+  { P_IGUANA,        "iguana",        _IGUANA,    "iguana",                  "" },
   { P_ISA_L,         "igzip",         _ISA_L,     "igzip",                   "0,1,2,3" },
   
   { P_KANZI,         "kanzi",         _KANZI,     "kanzi",                   "0,1,2,3,4,5,6,7,8,9/T#" },
@@ -2246,6 +2313,9 @@ unsigned codcomp(unsigned char *in, unsigned inlen, unsigned char *out, unsigned
     case P_HEATSHRINK:   return hscompress(in, inlen, out);
       #endif
 
+      #if _IGUANA
+    case P_IGUANA:  return IguanaComp(in, inlen, out);
+      #endif
 
       #if _ISA_L
     case P_ISA_L: struct isal_zstream s;
@@ -3196,6 +3266,10 @@ unsigned coddecomp(unsigned char *in, unsigned inlen, unsigned char *out, unsign
 
       #if _HEATSHRINK
     case P_HEATSHRINK: return hsdecompress(in, inlen, out, outlen);
+      #endif
+
+      #if _IGUANA
+    case P_IGUANA: return IguanaDecomp(in, inlen, out, outlen);
       #endif
 
       #if _ISA_L
